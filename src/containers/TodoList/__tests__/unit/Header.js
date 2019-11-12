89389869d81cfdb9e715fe54c9c0858c4cc6c4ca
zhanzhoudong <zhanzhoudong@adpanshi.com>
2019-11-12 11:17:30 +0800

    '优化测试提示'

--- src/containers/TodoList/__tests__/unit/Header.js
+++ src/containers/TodoList/__tests__/unit/Header.js
@@ -8,92 +8,94 @@ import { findTestWrapper } from '../../../../utils/testUtils'
 
 // Enzyme.configure({ adapter: new Adapter() });
 // import ReactDOM from 'react-dom';
-
-it('header 渲染样式正常', () => {
-  const wrapper = shallow(<Header />)
-  expect(wrapper).toMatchSnapshot()
-  // const inputElem = wrapper.find("[data-test='input']")
-  // expect(inputElem.length).toBe(1)
+descibe("Header 组件", () => {
+  it('渲染样式正常', () => {
+    const wrapper = shallow(<Header />)
+    expect(wrapper).toMatchSnapshot()
+    // const inputElem = wrapper.find("[data-test='input']")
+    // expect(inputElem.length).toBe(1)
+    
+  });
   
-});
-
-it('header 组件包含一个input 框', () => {
-  const wrapper = shallow(<Header />)
-  // const inputElem = wrapper.find("[data-test='input']")
-  const inputElem = findTestWrapper(wrapper, 'input')
-  expect(inputElem.length).toBe(1)
+  it('组件包含一个input 框', () => {
+    const wrapper = shallow(<Header />)
+    // const inputElem = wrapper.find("[data-test='input']")
+    const inputElem = findTestWrapper(wrapper, 'input')
+    expect(inputElem.length).toBe(1)
+    
+  });
   
-});
-
-it('header 组件 input框内容 初始化应该为空', () => {
-  const wrapper = shallow(<Header />)
-  // const inputElem = wrapper.find("[data-test='input']")
-  const inputElem = findTestWrapper(wrapper, 'input')
-  expect(inputElem.prop('value')).toEqual('')
+  it('input框内容 初始化应该为空', () => {
+    const wrapper = shallow(<Header />)
+    // const inputElem = wrapper.find("[data-test='input']")
+    const inputElem = findTestWrapper(wrapper, 'input')
+    expect(inputElem.prop('value')).toEqual('')
+    
+  });
   
-});
-
-it('header 组件 input框内容 当用户输入时跟随变化', () => {
-  const wrapper = shallow(<Header />)
-  // const inputElem = wrapper.find("[data-test='input']")
-  const inputElem = findTestWrapper(wrapper, 'input')
-  // expect(inputElem.prop('value')).toEqual('')
-  const userInput = '今天要学习jest'
-  inputElem.simulate('change', {
-    target: {
-      value: userInput
-    }
+  it('header 组件 input框内容 当用户输入时跟随变化', () => {
+    const wrapper = shallow(<Header />)
+    // const inputElem = wrapper.find("[data-test='input']")
+    const inputElem = findTestWrapper(wrapper, 'input')
+    // expect(inputElem.prop('value')).toEqual('')
+    const userInput = '今天要学习jest'
+    inputElem.simulate('change', {
+      target: {
+        value: userInput
+      }
+    })
+    expect(wrapper.state('value')).toEqual(userInput)
+    const newInputElem = wrapper.find("[data-test='input']")
+    expect(newInputElem.prop('value')).toBe(userInput)
+  });
+  
+  it('header组件 input 框输入回车时,如果input 无内容， 无操作', () => {
+    const fn = jest.fn()
+    const wrapper = shallow(<Header addUndoItem={fn}/>)
+    // const inputElem = wrapper.find("[data-test='input']")
+    const inputElem = findTestWrapper(wrapper, 'input')
+    // const value = ''
+    wrapper.setState({
+      value: ''
+    })
+    inputElem.simulate('keyUp', {
+      keyCode: 13
+    })
+    expect(fn).not.toHaveBeenCalled()
   })
-  expect(wrapper.state('value')).toEqual(userInput)
-  const newInputElem = wrapper.find("[data-test='input']")
-  expect(newInputElem.prop('value')).toBe(userInput)
-});
-
-it('header组件 input 框输入回车时,如果input 无内容， 无操作', () => {
-  const fn = jest.fn()
-  const wrapper = shallow(<Header addUndoItem={fn}/>)
-  // const inputElem = wrapper.find("[data-test='input']")
-  const inputElem = findTestWrapper(wrapper, 'input')
-  // const value = ''
-  wrapper.setState({
-    value: ''
+  
+  it('header组件 input 框输入回车时,如果input 有内容， 有操作', () => {
+    const fn = jest.fn()
+    const wrapper = shallow(<Header addUndoItem={fn}/>)
+    // const inputElem = wrapper.find("[data-test='input']")
+    const inputElem = findTestWrapper(wrapper, 'input')
+    // const value = ''
+    wrapper.setState({
+      value: '学习测试'
+    })
+    inputElem.simulate('keyUp', {
+      keyCode: 13
+    })
+    expect(fn).toHaveBeenCalled()
+    expect(fn).toHaveBeenLastCalledWith('学习测试')
   })
-  inputElem.simulate('keyUp', {
-    keyCode: 13
+  
+  it('header组件 input 框输入回车时,如果input 有内容， 最后应该清除掉', () => {
+    const fn = jest.fn()
+    const wrapper = shallow(<Header addUndoItem={fn}/>)
+    // const inputElem = wrapper.find("[data-test='input']")
+    const inputElem = findTestWrapper(wrapper, 'input')
+    // const value = ''
+    wrapper.setState({
+      value: '学习测试'
+    })
+    inputElem.simulate('keyUp', {
+      keyCode: 13
+    })
+    const newInputElem = wrapper.find("[data-test='input']")
+    expect(newInputElem.prop('value')).toBe('')
+    // expect(fn).toHaveBeenCalled()
+    // expect(fn).toHaveBeenLastCalledWith('学习测试')
   })
-  expect(fn).not.toHaveBeenCalled()
 })
 
-it('header组件 input 框输入回车时,如果input 有内容， 有操作', () => {
-  const fn = jest.fn()
-  const wrapper = shallow(<Header addUndoItem={fn}/>)
-  // const inputElem = wrapper.find("[data-test='input']")
-  const inputElem = findTestWrapper(wrapper, 'input')
-  // const value = ''
-  wrapper.setState({
-    value: '学习测试'
-  })
-  inputElem.simulate('keyUp', {
-    keyCode: 13
-  })
-  expect(fn).toHaveBeenCalled()
-  expect(fn).toHaveBeenLastCalledWith('学习测试')
-})
-
-it('header组件 input 框输入回车时,如果input 有内容， 最后应该清除掉', () => {
-  const fn = jest.fn()
-  const wrapper = shallow(<Header addUndoItem={fn}/>)
-  // const inputElem = wrapper.find("[data-test='input']")
-  const inputElem = findTestWrapper(wrapper, 'input')
-  // const value = ''
-  wrapper.setState({
-    value: '学习测试'
-  })
-  inputElem.simulate('keyUp', {
-    keyCode: 13
-  })
-  const newInputElem = wrapper.find("[data-test='input']")
-  expect(newInputElem.prop('value')).toBe('')
-  // expect(fn).toHaveBeenCalled()
-  // expect(fn).toHaveBeenLastCalledWith('学习测试')
-})
--- src/containers/TodoList/components/UndoList.js
+++ src/containers/TodoList/components/UndoList.js
@@ -5,14 +5,18 @@ export default class UndoList extends Component {
   render() {
     const { list, deleteItem } = this.props
     return (
-      <div>
-        <div data-test="count">{list.length}</div>
-        <ul>
+      <div className="undo-list">
+        <div className="undo-list-title">
+          正在进行
+          <div data-test="count" className="undo-list-count">{list.length}</div>
+        </div>
+        
+        <ul className="undo-list-content">
           {
             list.map((item, index) => (
-              <li data-test="list-item" key={`${item}-${index}`}>
+              <li className="undo-list-item" data-test="list-item" key={`${item}-${index}`}>
                 {item}
-                <span data-test="delete-item" onClick={() => {
+                <span className="undo-list-delete" data-test="delete-item" onClick={() => {
                   deleteItem(index)
                 }}>-</span>
               </li>
